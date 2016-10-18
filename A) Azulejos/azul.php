00c4384b1e01663d6cf18900be3c6c947e965a11
Pliavi <pliavi.rpgm@gmail.com>
2016-10-18 00:46:24 -0200

    finalizado

---
+++ A) Azulejos/azul.php
@@ -0,0 +1,30 @@
+<?php
+$in = fopen("azul.in", "r");
+$out = fopen("azul.out", "w");
+
+while(($line = fgets($in))[0] != 0){
+  $azulejosTotal = $line;
+  $conjuntos = 0;
+  $azulejos = intval($azulejosTotal);
+
+  while($azulejos > 3){
+    $raiz = sqrt($azulejos);
+    if(!strpos($raiz, '.')){
+      $azulejos = $azulejosTotal - sqrt($azulejos)*sqrt($azulejos);
+      $azulejosTotal = $azulejos;
+      $conjuntos++;
+    } else {
+      $azulejos -= 1;
+    }
+  }
+
+  $conjuntos += $azulejos;
+
+  echo "$conjuntos<br>";
+
+  fwrite($out, "$conjuntos\n");
+}
+
+fclose($out);
+fclose($in);
+?>
